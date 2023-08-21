02ed67d409849ca5bdb48c2b00b0b9c05b4c426e
Jiiku831 <142133559+Jiiku831@users.noreply.github.com>
2023-08-21 02:23:15 -0700

    update

--- stonks.js
+++ stonks.js
@@ -10,41 +10,25 @@ function Get(x) {
     return parseFloat(document.getElementById(x).value);
 }
 
-function Val(a, b, c, d, e, f, g) {
-    return b * e +
-        a * b * (f + g * (0.002 * c + 0.008 * d));
+function Val(a, b, c, d, e, f, g, h, w, z) {
+    return b * f +
+        a * b * z * (g + h * (0.002 * c + 0.008 * d) + e * w);
 }
 
-function AB(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f, g);
-    return -500 * s / (b ** 2 * (g * (c + 4 * d) + 500 * f));
+function BA(a, b, c, d, e, f, g, h, w, z) {
+    let s = Val(a, b, c, d, e, f, g, h, w, z);
+    return -500 * s * z * (c * h + 4 * d * h + 500 * e * w + 500 * g) /
+        (a * z * (c * h + 4 * d * h + 500 * e * w + 500 * g) + 500 * f) ** 2;
 }
 
-function AC(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f);
-    return -500 * g * (b * e - s) / b * (g * (c + 4 * d) + 500 * f) ** 2;
+function CA(a, b, c, d, e, f, g, h, w, z) {
+    let s = Val(a, b, c, d, e, f, g, h, w, z);
+    return 500 * (b * f - s) / (a ** 2 * b * h * z);
 }
 
-function BA(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f, g);
-    return -500 * s * (g * (c + 4 * d) + 500 * f) /
-        (a * g * (c + 4 * d) + 500 * a * f + 500 * e) ** 2;
-}
-
-function BC(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f, g);
-    return -500 * a * g * s /
-        (a * f * (c + 4 * d) + 500 * a * f + 500 * e) ** 2;
-}
-
-function CA(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f, g);
-    return 500 * (b * e - s) / (a ** 2 * b * g);
-}
-
-function CB(a, b, c, d, e, f, g) {
-    let s = Val(a, b, c, d, e, f, g);
-    return -500 * s / (a * b ** 2 * g);
+function CB(a, b, c, d, e, f, g, h, w, z) {
+    let s = Val(a, b, c, d, e, f, g, h, w, z);
+    return -500 * s / (a * b ** 2 * h * z);
 }
 
 function Clear(e) {
@@ -89,17 +73,17 @@ function L(fn, m, x, y, x1, x2, y1, y2) {
 }
 
 function PlotFn(
-    out, fn, w, h, x, y, x1, x2, y1, y2, a, b, c, d, e, f, g, m, lx, ly) {
+    out, fn, pw, ph, x, y, x1, x2, y1, y2, a, b, c, d, e, f, g, h, w, z, m, lx, ly) {
     let t = T(fn, x1, x2, y1, y2);
-    let l = L(fn, m(a, b, c, d, e, f, g), x, y, x1, x2, y1, y2);
+    let l = L(fn, m(a, b, c, d, e, f, g, h, w, z), x, y, x1, x2, y1, y2);
     let p = Plot.plot({
         color: {
             scheme: "Blues",
             legend: true,
             label: "Event Points",
         },
-        height: h,
-        width: w,
+        height: ph,
+        width: pw,
         marks: [
             Plot.contour({
                 fill: (x, y) => fn(x, y) / 100,
@@ -125,8 +109,8 @@ function PlotFn(
     cc.append(p);
 }
 
-function SumI(a, b, c, d, e, f, g, m, dx, dy, ux, uy) {
-    let fc = m(a, b, c, d, e, f, g) * -1;
+function SumI(a, b, c, d, e, f, g, h, w, z, m, dx, dy, ux, uy) {
+    let fc = m(a, b, c, d, e, f, g, h, w, z) * -1;
     return "<table><tr>" +
         `<td>${dx}${ux}</td><td>&approx;</td><td>${(fc * dx).toFixed(1)}${uy}` +
         "</td></tr><tr>" +
@@ -134,9 +118,9 @@ function SumI(a, b, c, d, e, f, g, m, dx, dy, ux, uy) {
         "</td></tr></table>";
 }
 
-function Sum(out, a, b, c, d, e, f, g, m, dx, dy, ux, uy) {
+function Sum(out, a, b, c, d, e, f, g, h, w, z, m, dx, dy, ux, uy) {
     let cc = document.getElementById(out);
-    cc.innerHTML = SumI(a, b, c, d, e, f, g, m, dx, dy, ux, uy);
+    cc.innerHTML = SumI(a, b, c, d, e, f, g, h, w, z, m, dx, dy, ux, uy);
 }
 
 function Tr(row, data, tag, span) {
@@ -149,7 +133,7 @@ function Tr(row, data, tag, span) {
     }
 }
 
-function Table(out, a, b, c, d, cs) {
+function Table(out, a, b, c, d, e, z, cs, cl) {
     let cc = document.getElementById(out);
     Clear(cc);
     let table = document.createElement("table");
@@ -165,13 +149,13 @@ function Table(out, a, b, c, d, cs) {
         "Avg. EP<small><sup>&ddagger;</sup></small>", "BP &harr; EB",
         "BP &harr; SV", "EB &harr; SV",
     ], "th");
-    let f = (...x) => Math.round(Val(a, b, ...x) / 100).toLocaleString();
+    let f = (...x) => Math.round(Val(a, b, ...x, z) / 100).toLocaleString();
     let f1 = (...x) =>
-        SumI(a, b, ...x, BA, 10000, 10, " BP", "% EB");
+        SumI(a, b, ...x, z, BA, 10000, 10, " BP", "% EB");
     let f2 = (...x) =>
-        SumI(a, b, ...x, CA, 1000, 10, " BP", "% SV");
+        SumI(a, b, ...x, z, CA, 1000, 10, " BP", "% SV");
     let f3 = (...x) =>
-        SumI(a, b, ...x, CB, 1, 10, "% EB", "% SV");
+        SumI(a, b, ...x, z, CB, 1, 10, "% EB", "% SV");
     for (let i = 0; i < params[0].length; ++i) {
         data = [
             params[0][i][0],
@@ -179,14 +163,15 @@ function Table(out, a, b, c, d, cs) {
         for (let j = 0; j < params.length; ++j) {
             let nc = j == 2 ? cs : c;
             let nd = j == 2 ? cs : d;
+            let ne = j == 2 ? cl / 100: e;
             data.push(
-                f(nc, nd, params[j][i][1], params[j][i][2], params[j][i][3]));
+                f(nc, nd, ne, params[j][i][1], params[j][i][2], params[j][i][3], params[j][i][4]));
             data.push(
-                f1(nc, nd, params[j][i][1], params[j][i][2], params[j][i][3]));
+                f1(nc, nd, ne, params[j][i][1], params[j][i][2], params[j][i][3], params[j][i][4]));
             data.push(
-                f2(nc, nd, params[j][i][1], params[j][i][2], params[j][i][3]));
+                f2(nc, nd, ne, params[j][i][1], params[j][i][2], params[j][i][3], params[j][i][4]));
             data.push(
-                f3(nc, nd, params[j][i][1], params[j][i][2], params[j][i][3]));
+                f3(nc, nd, ne, params[j][i][1], params[j][i][2], params[j][i][3], params[j][i][4]));
         }
         Tr(table.insertRow(), data, "td");
     }
@@ -195,48 +180,58 @@ function Table(out, a, b, c, d, cs) {
 
 const params = [
     [
-        ["Random Ex", 141.6392308, 0.0003462749307, 0.00009017817953],
-        ["Random Mas", 141.6392308, 0.0003546738204, 0.00009229621155],
-        ["2:00+ Ex", 145.2796507, 0.0003573538288, 0.00008547107657],
-        ["2:00+ Mas", 145.2796507, 0.00036590857, 0.00008715137814],
-        ["2:20+ Ex", 149.0236111, 0.0003689485029, 0.00008126597847],
-        ["2:20+ Mas", 149.0236111, 0.0003777056243, 0.00008280224411],
-        ["Ebi Hard", 123, 0.0002806425011, 0.000111870182],
-        ["Ebi Ex", 123, 0.0002892108452, 0.0001249630476],
-        ["Ebi Mas", 123, 0.0002996182266, 0.0001227714286],
-        ["L&F Hard", 150.06, 0.0003802653103, 0.000112988413],
-        ["L&F Ex", 150.06, 0.0003862777021, 0.0001081331092],
-        ["L&F Mas", 150.06, 0.0003873623638, 0.00009288017109],
+        ["Random Ex", 141.5471052631579, 0.00034605372824218826, 7.342056934497954e-05, 1.6899812340557115e-05],
+        ["Random Mas", 141.55758017492712, 0.0003544608603805854, 7.494942753428044e-05, 1.7502404941490973e-05],
+        ["2:00+ Ex", 145.27965065502184, 0.0003573538288360647, 6.880684540451749e-05, 1.666423116117773e-05],
+        ["2:00+ Mas", 145.27904347826086, 0.0003658898696533362, 6.990290685507412e-05, 1.7285551356886923e-05],
+        ["2:20+ Ex", 149.0236111111111, 0.0003689485029340017, 6.4976448857753e-05, 1.6289529611553315e-05],
+        ["2:20+ Mas", 149.0236111111111, 0.00037770562434199183, 6.599484193002225e-05, 1.6807402176334123e-05],
+        ["Ebi Hard", 123.0, 0.00028064250105797744, 7.716903089293271e-05, 3.47011510791367e-05],
+        ["Ebi Ex", 123.0, 0.0002892108452090447, 8.760748540534706e-05, 3.7355562146930754e-05],
+        ["Ebi Mas", 123.0, 0.00029961822660098524, 8.941970443349751e-05, 3.335172413793104e-05],
+        ["L&F Hard", 150.06, 0.00038026531032788965, 9.444643558884933e-05, 1.8541977403905194e-05],
+        ["L&F Ex", 150.06, 0.00038627770214701124, 9.471546302010593e-05, 1.3417646149348745e-05],
+        ["L&F Mas", 150.06, 0.00038736236379462066, 7.791182249366639e-05, 1.4968348593963504e-05],
     ],
     [
-        ["Random Ex", 191.21296158, 0.00046747115644500003, 0.0001217405423655],
-        ["Random Mas", 191.21296158, 0.0004788096575400000, 0.0001245998855925],
-        ["2:00+ Ex", 196.127528445, 0.00048242766888000006, 0.0001153859533695],
-        ["2:00+ Mas", 196.127528445, 0.0004939765695, 0.00011765436048900001],
-        ["2:20+ Ex", 201.18187498500004, 0.000498080478915, 0.0001097090709345],
-        ["2:20+ Mas", 201.18187498500004, 0.00050990259280, 0.0001117830295485],
-        ["Ebi Hard", 166.05, 0.000378867376485, 0.00015102474570000002],
-        ["Ebi Ex", 166.05, 0.00039043464102000004, 0.00016870011426000002],
-        ["Ebi Mas", 166.05, 0.00040448460591000006, 0.00016574142861000001],
-        ["L&F Hard", 202.58100000000002, 0.000513358168905, 0.00015253435755],
-        ["L&F Ex", 202.58100000000002, 0.0005214748978350001, 0.00014597969742],
-        ["L&F Mas", 202.58100000000002, 0.00052293919113, 0.000125388230971500],
+        ["Random Ex", 191.08859210526316, 0.0004671725331269542, 9.911776861572237e-05, 2.281474665975211e-05],
+        ["Random Mas", 191.1027332361516, 0.0004785221615137904, 0.0001011817271712786, 2.3628246671012814e-05],
+        ["2:00+ Ex", 196.1275283842795, 0.0004824276689286873, 9.28892412960986e-05, 2.2496712067589933e-05],
+        ["2:00+ Mas", 196.12670869565218, 0.000493951324032004, 9.436892425435006e-05, 2.3335494331797348e-05],
+        ["2:20+ Ex", 201.18187500000002, 0.0004980804789609023, 8.771820595796657e-05, 2.1990864975596978e-05],
+        ["2:20+ Mas", 201.18187500000002, 0.0005099025928616891, 8.909303660553004e-05, 2.2689992938051072e-05],
+        ["Ebi Hard", 166.05, 0.00037886737642826965, 0.00010417819170545916, 4.684655395683455e-05],
+        ["Ebi Ex", 166.05, 0.0003904346410322104, 0.00011827010529721854, 5.0430008898356515e-05],
+        ["Ebi Mas", 166.05, 0.00040448460591133005, 0.00012071660098522166, 4.502482758620691e-05],
+        ["L&F Hard", 202.58100000000002, 0.0005133581689426511, 0.00012750268804494658, 2.5031669495272014e-05],
+        ["L&F Ex", 202.58100000000002, 0.0005214748978984653, 0.00012786587507714303, 1.8113822301620805e-05],
+        ["L&F Mas", 202.58100000000002, 0.0005229391911227379, 0.00010518096036644963, 2.020727060185073e-05],
     ],
     [
-        ["Random Ex", 115.1538462, 0.0002943336911, 0.00007020595503],
-        ["Random Mas", 115.1538462, 0.0003014727473, 0.00007188613486],
-        ["2:00+ Ex", 118.1135371, 0.0003037507545, 0.00006648455822],
-        ["2:00+ Mas", 118.1135371, 0.0003110222845, 0.00006782307629],
-        ["2:20+ Ex", 121.1574074, 0.0003136062275, 0.00006300645713],
-        ["2:20+ Mas", 121.1574074, 0.0003210497807, 0.00006423110068],
-        ["Ebi Hard", 100, 0.0002385461259, 0.00008525766187],
-        ["Ebi Ex", 100, 0.0002458292184, 0.00009737595233],
-        ["Ebi Mas", 100, 0.0002546754926, 0.00009490605911],
-        ["L&F Hard", 122, 0.0003232255138, 0.00008711820063],
-        ["L&F Ex", 122, 0.0003283360468, 0.00008386203203],
-        ["L&F Mas", 122, 0.0003292580092, 0.00007172826268],
+        ["Random Ex", 115.07894736842105, 0.00026381732985138255, 5.753392643387819e-05, 1.2766247037636025e-05],
+        ["Random Mas", 115.08746355685132, 0.0002707480416975781, 5.874401840104312e-05, 1.324926406299535e-05],
+        ["2:00+ Ex", 118.11353711790393, 0.00027202388715991447, 5.429820765262187e-05, 1.2186350565014892e-05],
+        ["2:00+ Mas", 118.11304347826086, 0.0002792679791600689, 5.5162513157280074e-05, 1.2690008078829523e-05],
+        ["2:20+ Ex", 121.1574074074074, 0.0002801857821319739, 5.131222502379771e-05, 1.1694232109852208e-05],
+        ["2:20+ Mas", 121.1574074074074, 0.0002875802694565543, 5.2128979029456625e-05, 1.2102121649037972e-05],
+        ["Ebi Hard", 100.0, 0.00021392743525179884, 6.55936762589928e-05, 1.9663985611510787e-05],
+        ["Ebi Ex", 100.0, 0.00022199652074260894, 7.446636259454501e-05, 2.290958973183588e-05],
+        ["Ebi Mas", 100.0, 0.00022663135467980298, 7.600674876847289e-05, 1.8899310344827575e-05],
+        ["L&F Hard", 122.0, 0.0002707480797494786, 7.135751983298539e-05, 1.5760680793319415e-05],
+        ["L&F Ex", 122.0, 0.0002824089683048049, 7.245703279955828e-05, 1.1404999226946433e-05],
+        ["L&F Mas", 122.0, 0.00028957830443911695, 5.900516637182773e-05, 1.2723096304868978e-05],
     ],
-]
+];
+
+function ESV() {
+    let ct = Get("ct");
+    let cl = Get("cl");
+    let c = cl + (ct - cl) / 5;
+    let d = Get("d");
+    let e = (c + 4 * d) / 5;
+    document.getElementById("es").value = e;
+    document.getElementById("e").value = e.toFixed(2);
+}
 
 function Run() {
     let a = Get("a");
@@ -245,54 +240,58 @@ function Run() {
     let cl = Get("cl");
     let c = cl + (ct - cl) / 5;
     let d = Get("d");
+    let e = Get("e") / 100;
     let s = parseInt(document.querySelector("input[name=\"s\"]:checked").value);
     let m = parseInt(document.querySelector("input[name=\"m\"]:checked").value);
     if (m == 2) {
         d = c = ct / 5;
+        e = cl / 100;
     }
-    let e = params[m][s][1];
-    let f = params[m][s][2];
-    let g = params[m][s][3];
+    let f = params[m][s][1];
+    let g = params[m][s][2];
+    let h = params[m][s][3];
+    let w = params[m][s][4];
+    let z = Get("z") / 100;
 
     document.getElementById("v").innerText =
-        Math.round(Val(a, b, c, d, e, f, g) / 100).toLocaleString();
+        Math.round(Val(a, b, c, d, e, f, g, h, w, z) / 100).toLocaleString();
 
-    let fn1 = (x, y) => Val(x, y, c, d, e, f, g);
-    let fn2 = (x, y) => Val(x, b, y, d, e, f, g);
-    let fn3 = (x, y) => Val(a, x, y, d, e, f, g);
+    let fn1 = (x, y) => Val(x, y, c, d, e, f, g, h, w, z);
+    let fn2 = (x, y) => Val(x, b, y, d, e, f, g, h, w, z);
+    let fn3 = (x, y) => Val(a, x, y, d, e, f, g, h, w, z);
 
     PlotFn("plota", fn1, 600, 600,
         a, b,
         Math.max(0, a - 50000), a + 50000,
         Math.max(100, b - 50), Math.min(b + 50, 500),
-        a, b, c, d, e, f, g, BA, "Team Power", "Event Bonus");
+        a, b, c, d, e, f, g, h, w, z, BA, "Team Power", "Event Bonus");
     PlotFn("plotb", fn2, 600, 600,
         a, c,
         Math.max(0, a - 50000), a + 50000,
         Math.max(0, c - 50), Math.min(c + 50, 300),
-        a, b, c, d, e, f, g, CA, "Team Power", "Skill Value");
+        a, b, c, d, e, f, g, h, w, z, CA, "Team Power", "Skill Value");
     PlotFn("plotc", fn3, 600, 600,
         b, c,
         Math.max(100, b - 50), Math.min(b + 50, 500),
         Math.max(0, c - 50), Math.min(c + 50, 300),
-        a, b, c, d, e, f, g, CB, "Event Bonus", "Skill Value");
+        a, b, c, d, e, f, g, h, w, z, CB, "Event Bonus", "Skill Value");
     PlotFn("plota-over", fn1, 600, 600,
         a, b,
         0, 375000, 100, 500,
-        a, b, c, d, e, f, g, BA, "Team Power", "Event Bonus");
+        a, b, c, d, e, f, g, h, w, z, BA, "Team Power", "Event Bonus");
     PlotFn("plotb-over", fn2, 600, 600,
         a, c,
         0, 375000, 0, 300,
-        a, b, c, d, e, f, g, CA, "Team Power", "Skill Value");
+        a, b, c, d, e, f, g, h, w, z, CA, "Team Power", "Skill Value");
     PlotFn("plotc-over", fn3, 600, 600,
         b, c,
         100, 500, 0, 300,
-        a, b, c, d, e, f, g, CB, "Event Bonus", "Skill Value");
-    Sum("suma", a, b, c, d, e, f, g, BA, 10000, 10,
+        a, b, c, d, e, f, g, h, w, z, CB, "Event Bonus", "Skill Value");
+    Sum("suma", a, b, c, d, e, f, g, h, w, z, BA, 10000, 10,
         " Team Power", "% Event Bonus");
-    Sum("sumb", a, b, c, d, e, f, g, CA, 1000, 10,
+    Sum("sumb", a, b, c, d, e, f, g, h, w, z, CA, 1000, 10,
         " Team Power", "% Skill Value");
-    Sum("sumc", a, b, c, d, e, f, g, CB, 1, 10,
+    Sum("sumc", a, b, c, d, e, f, g, h, w, z, CB, 1, 10,
         "% Event Bonus", "% Skill Value");
-    Table("sumt", a, b, c, d, ct / 5);
+    Table("sumt", a, b, c, d, e, z, ct / 5, cl);
 }
