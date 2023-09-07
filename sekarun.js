1f9394d01d95c7433d055ad739581ccb2b220940
Jiiku831 <142133559+Jiiku831@users.noreply.github.com>
2023-09-07 01:54:12 -0700

    update

--- sekarun.js
+++ sekarun.js
@@ -18,14 +18,22 @@ function P(e, b) {
     document.getElementById(e).appendChild(
         Plot.plot({
             height: 800,
-            width: 1200,
+            width: 1400,
             style: "overflow: visible;",
             color: {legend: true},
             y: {grid: true},
             x: {grid: true},
             marks: [
+                Plot.axisX({anchor: "bottom", label: "Time From End (hrs)"}),
+                Plot.axisY({anchor: "left", label: "Event Points"}),
+                Plot.axisY({anchor: "right", label: "Event Points"}),
                 Plot.ruleY([0]),
-                Plot.ruleX([0]),
+                Plot.ruleX([0], {
+                    stroke: "#666",
+                    strokeOpacity: 0.5,
+                    strokeDasharray: "3 2",
+                }),
+                Plot.ruleX([18], {strokeOpacity: 0}),
                 Plot.ruleX(
                     [timeToCurrentEventEnd],
                     {
@@ -95,7 +103,19 @@ function P(e, b) {
                         strokeDasharray: "4 3",
                     }),
                 Plot.crosshair(
-                    filteredData, {x: "tfe", y: "ep"}),
+                    filteredData.filter(
+                        e => e.eid != currentEvent && e.t == "i"), {
+                        x: "tfe",
+                        y: "ep",
+                        maxRadius: 10,
+                    }),
+                Plot.crosshairX(
+                    filteredData.filter(
+                        e => e.eid == currentEvent && e.t == "i" || e.t == "p"),
+                    {
+                        x: "tfe",
+                        y: "ep",
+                    }),
                 Plot.text(filteredData,
                     Plot.selectLast({
                         filter: e => e.t == "r",
@@ -104,7 +124,7 @@ function P(e, b) {
                         z: "eid",
                         text: e => events[e.eid].nick,
                         textAnchor: "start",
-                        dx: 5
+                        dx: 5,
                     })),
                 Plot.text(
                     [[timeToCurrentEventEnd, 0]],
@@ -127,6 +147,7 @@ function P(e, b) {
                         },
                         anchor: "bottom-right",
                         tip: true,
+                        maxRadius: 10,
                     })),
                 Plot.line(
                     Object.keys(events).map(x => ({eid: x})),
