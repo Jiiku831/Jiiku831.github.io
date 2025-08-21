790a0101a968774403834d7772b5cc022aaf97ec
JiikuBot <142133559+Jiiku831@users.noreply.github.com>
2025-08-21 13:01:19 +0000

    Border update Thu Aug 21 13:01:19 UTC 2025

--- tw/data/sakirun.js
+++ tw/data/sakirun.js
@@ -769,4 +769,4 @@ events = {
         "end": 1757336400.0,
         "len": 198
     }
-};data = [];currentEvent = "140";currentChapter = "140-2";
+};data = [];currentEvent = "140";currentChapter = "140-3";
